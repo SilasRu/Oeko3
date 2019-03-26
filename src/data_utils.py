a3e51635f65499fe029edbf265d12226e33afd4f
rudolsil <rudolsil@students.zhaw.ch>
2019-03-26 12:25:44 +0100

    Spectrogram conversion improvement

--- src/data_utils.py
+++ src/data_utils.py
@@ -5,34 +5,31 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-import os, fnmatch
+import os, glob
 import librosa
 from scipy import signal
 from scipy.io import wavfile
 import librosa.display
 
+
 # File directories
-data_filedir = os.path.abspath('..\\data')
+data_filedir = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'data', 'external', 'train')
 
 class DataUtils():
     def __init__(self, speaker):
         self.speaker = speaker
-        self.speaker_path = data_filedir + '\\external\\train\\'  + self.speaker + '\\'
-        self.speaker_png_path = self.speaker_path + 'spectrograms\\'
-        self.speaker_mel_png_path = self.speaker_path + 'mel_spectrograms\\'
+        self.speaker_path = os.path.join(data_filedir, self.speaker)
+        self.speaker_png_path = os.path.join(self.speaker_path, 'spectrograms')
+        self.speaker_mel_png_path = os.path.join(self.speaker_path , 'mel_spectrograms')
+
         
     def read_folder_content(self):
-        listOfFiles = os.listdir(self.speaker_path)
-        pattern = '*.wav'
-        available_files = {}
-        for entry in listOfFiles:
-            if fnmatch.fnmatch(entry, pattern):
-                available_files.update({'{}'.format(entry): entry})
+        available_files = glob.glob1(self.speaker_path, '*.wav')
         return available_files
     
     def original_spectrogram_conversion(self, file):
         filename = file.replace('.wav', '')
-        sample_rate, samples = wavfile.read(self.speaker_path + file)
+        sample_rate, samples = wavfile.read(os.path.join(self.speaker_path,file))
         frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
         plt.pcolormesh(times, frequencies, np.log(spectrogram))
         plt.imshow(np.log(spectrogram))
@@ -42,7 +39,7 @@ class DataUtils():
         
     def mel_spectrogram_conversion(self, file):
         filename = file.replace('.wav', '')
-        sample_rate, samples = librosa.load(self.speaker_path + file)
+        sample_rate, samples = librosa.load(os.path.join(self.speaker_path,  file))
         spectrogram = librosa.feature.melspectrogram(y=sample_rate, sr=samples, n_mels = 128, fmax= 8000)
         plt.figure(figsize=(10, 4))
         librosa.display.specshow(librosa.power_to_db(spectrogram, ref=np.max),
@@ -57,22 +54,28 @@ class DataUtils():
 
         
 
-utils = DataUtils('berset')
-utils.speaker_mel_png_path
-utils.read_folder_content()
+# =============================================================================
+# Usage
+# =============================================================================
 
+# Defining speaker
+utils = DataUtils('ritz')
+
+# Spectrogram conversion
 for i in utils.read_folder_content():
         utils.original_spectrogram_conversion(i)
         
-        
-sample_rate, samples = librosa.load('1_august_000.wav')
-spectrogram = librosa.feature.melspectrogram(y=sample_rate, sr=samples, n_mels = 128, fmax= 8000)
 
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(librosa.power_to_db(spectrogram,ref=np.max),
-                          y_axis='mel', fmax=8000,
-                          x_axis='time')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Mel spectrogram')
-plt.tight_layout()
-plt.show()
+
+        
+#sample_rate, samples = librosa.load('1_august_000.wav')
+#spectrogram = librosa.feature.melspectrogram(y=sample_rate, sr=samples, n_mels = 128, fmax= 8000)
+#
+#plt.figure(figsize=(10, 4))
+#librosa.display.specshow(librosa.power_to_db(spectrogram,ref=np.max),
+#                          y_axis='mel', fmax=8000,
+#                          x_axis='time')
+#plt.colorbar(format='%+2.0f dB')
+#plt.title('Mel spectrogram')
+#plt.tight_layout()
+#plt.show()
